6100310e747d1e5c1dd73ab1d0958f51bd83a98f
Yasmin Gonçalves <yasmingoncalvesrodrigues@hotmail.com>
2021-11-14 00:55:10 -0300

    Form6 Atualizado

--- Projeto/Form6.Designer.cs
+++ Projeto/Form6.Designer.cs
@@ -29,12 +29,231 @@ namespace Projeto
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonExcluir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxExclusao = new System.Windows.Forms.ComboBox();
+            this.labelOutro = new System.Windows.Forms.Label();
+            this.textBoxOutro = new System.Windows.Forms.TextBox();
+            this.buttonVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.label1.Location = new System.Drawing.Point(97, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID do Livro:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.button1.Location = new System.Drawing.Point(425, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.textBox1.Location = new System.Drawing.Point(188, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(290, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Excluir Livros";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.label3.Location = new System.Drawing.Point(72, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nome do Livro:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.textBox2.Location = new System.Drawing.Point(188, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(387, 27);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.textBox3.Location = new System.Drawing.Point(188, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(387, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.label4.Location = new System.Drawing.Point(86, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contribuidor:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.textBox4.Location = new System.Drawing.Point(188, 232);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(387, 27);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.label5.Location = new System.Drawing.Point(43, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Data de Publicação:";
+            // 
+            // buttonExcluir
+            // 
+            this.buttonExcluir.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.buttonExcluir.Location = new System.Drawing.Point(397, 356);
+            this.buttonExcluir.Name = "buttonExcluir";
+            this.buttonExcluir.Size = new System.Drawing.Size(178, 27);
+            this.buttonExcluir.TabIndex = 10;
+            this.buttonExcluir.Text = "Excluir";
+            this.buttonExcluir.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.label6.Location = new System.Drawing.Point(43, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Motivo de Exclusão:";
+            // 
+            // comboBoxExclusao
+            // 
+            this.comboBoxExclusao.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.comboBoxExclusao.FormattingEnabled = true;
+            this.comboBoxExclusao.Items.AddRange(new object[] {
+            "Documento Ilegível.",
+            "Livro não correspondente ao nome.",
+            "Falta de Páginas.",
+            "Outro"});
+            this.comboBoxExclusao.Location = new System.Drawing.Point(188, 273);
+            this.comboBoxExclusao.Name = "comboBoxExclusao";
+            this.comboBoxExclusao.Size = new System.Drawing.Size(387, 27);
+            this.comboBoxExclusao.TabIndex = 12;
+            this.comboBoxExclusao.SelectedIndexChanged += new System.EventHandler(this.Outro);
+            // 
+            // labelOutro
+            // 
+            this.labelOutro.AutoSize = true;
+            this.labelOutro.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.labelOutro.Location = new System.Drawing.Point(134, 321);
+            this.labelOutro.Name = "labelOutro";
+            this.labelOutro.Size = new System.Drawing.Size(50, 19);
+            this.labelOutro.TabIndex = 13;
+            this.labelOutro.Text = "Outro:";
+            this.labelOutro.Visible = false;
+            // 
+            // textBoxOutro
+            // 
+            this.textBoxOutro.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.textBoxOutro.Location = new System.Drawing.Point(188, 313);
+            this.textBoxOutro.Name = "textBoxOutro";
+            this.textBoxOutro.Size = new System.Drawing.Size(387, 27);
+            this.textBoxOutro.TabIndex = 14;
+            this.textBoxOutro.Visible = false;
+            // 
+            // buttonVoltar
+            // 
+            this.buttonVoltar.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F);
+            this.buttonVoltar.Location = new System.Drawing.Point(190, 356);
+            this.buttonVoltar.Name = "buttonVoltar";
+            this.buttonVoltar.Size = new System.Drawing.Size(179, 27);
+            this.buttonVoltar.TabIndex = 15;
+            this.buttonVoltar.Text = "Voltar";
+            this.buttonVoltar.UseVisualStyleBackColor = true;
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(687, 449);
+            this.Controls.Add(this.buttonVoltar);
+            this.Controls.Add(this.textBoxOutro);
+            this.Controls.Add(this.labelOutro);
+            this.Controls.Add(this.comboBoxExclusao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonExcluir);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Name = "Form6";
             this.Text = "Form6";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonExcluir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxExclusao;
+        private System.Windows.Forms.Label labelOutro;
+        private System.Windows.Forms.TextBox textBoxOutro;
+        private System.Windows.Forms.Button buttonVoltar;
     }
 }
--- Projeto/Form6.cs
+++ Projeto/Form6.cs
@@ -16,5 +16,29 @@ namespace Projeto
         {
             InitializeComponent();
         }
+
+        private void Outro(object sender, EventArgs e)
+        {
+            if(comboBoxExclusao.Text.ToString() == "Outro")
+            {
+                labelOutro.Visible = true;
+                textBoxOutro.Visible = true;
+                buttonExcluir.Location = new Point(397, 356);
+                buttonVoltar.Location = new Point(190, 356);
+            }
+            else
+            {
+                labelOutro.Visible = false;
+                textBoxOutro.Visible = false;
+                buttonExcluir.Location = new Point(397, 313);
+                buttonVoltar.Location = new Point(190, 313);
+            }
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            buttonExcluir.Location = new Point(397, 313);
+            buttonVoltar.Location = new Point(190, 313);
+        }
     }
 }
